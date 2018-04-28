503b6c81fb175356425f3b0e54a8f50f48770b54
bryce <hibryce@gmail.com>
2018-04-28 18:04:34 -0500

    rename

--- charlie/preload.js
+++ charlie/preload.js
@@ -18,7 +18,7 @@
 
   // create overlay
   var o = document.createElement('div')
-  o.innerHTML = 'tap screen to load game'
+  o.innerHTML = 'tap to load'
   o.style.cssText = [
     'position: fixed',
     'top: 0',
